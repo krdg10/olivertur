dbc8c1090b7ee119c37cb5c55112e82966bee2e5
Kelvin Rodrigues <krdg10@live.com>
2019-09-03 08:48:56 -0400

    Update 2019_09_02_184330_create_posts_table.php

--- database/migrations/2019_09_02_184330_create_posts_table.php
+++ database/migrations/2019_09_02_184330_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('texto');
             $table->integer('user_id')->unsigned();
             $table->string('tag');
+            $table->date('data');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
