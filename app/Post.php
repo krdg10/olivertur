7bc513cec718d49e1e174965f650d9d38bd093a4
Kelvin Rodrigues <krdg10@live.com>
2019-09-03 08:57:17 -0400

    Update Post.php

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    // IRÁ PROCURAR O USER ASSOCIADO AO POST
     public function autor(){
-        return $this->hasOne(\App\User::class);
-    }
+    	return $this->hasOne(\App\User::class, 'id', 'user_id');
+	}
 }
