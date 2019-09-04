345645eac0c5b5121f2ebb9d0950594be953b3bb
krdg10 <krdg10@live.com>
2019-09-04 10:26:24 -0400

    posts aparecendo na home. tratar imagem.

--- app/Post.php
+++ app/Post.php
@@ -9,5 +9,8 @@ class Post extends Model
     // IRÁ PROCURAR O USER ASSOCIADO AO POST
     public function autor(){
     	return $this->hasOne(\App\User::class, 'id', 'user_id');
+    }
+    public function fotos(){
+    	return $this->hasOne(Foto::class);
 	}
 }
