b29c6fab70225f5dc6c04d98031890aaaef48221
krdg10 <krdg10@live.com>
2019-09-04 11:16:04 -0400

    nomes genericos

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,7 +5,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Olivertur</title>
+    <title>Exemplo</title>
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
