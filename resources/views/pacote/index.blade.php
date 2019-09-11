c3d515b38269709f72d98eb6ede28c3a6b2d1307
krdg10 <krdg10@live.com>
2019-09-11 17:08:50 -0400

    card de img de pacotes e editar post

--- resources/views/pacote/index.blade.php
+++ resources/views/pacote/index.blade.php
@@ -4,20 +4,22 @@
 <div class="container">
     <div class="row">
         @foreach($pacotes as $pacote)
-            <div class="card mb-4">
-                <a href="/pacotes/{{$pacote->id}}"><img class="card-img-top" src="/storage/{{ $pacote->thumbnail->url }}" alt="Card image cap"></a>
-                <div class="card-body">
-                    <a href="/pacotes/{{$pacote->id}}"><h5 class="card-title">{{ $pacote->nome }}</h5></a>
-                    <p class="card-text"><small class="text-muted">{{ $pacote->caracteristica1 }}</small></p>
-                    <p class="card-text"><small class="text-muted">{{ $pacote->caracteristica2 }}</small></p>
-                    <p class="card-text"><small class="text-muted">{{ $pacote->caracteristica3 }}</small></p>
-                    <p class="card-text">{{$pacote->preco}} em até {{ $pacote->parcelas }}x</p>
-                    @guest
-                        @if (Route::has('register'))
-                        @endif
-                        @else
-                        <a href="/pacotes/edit/{{$pacote->id}}"><i class="fas fa-edit"></i> Editar/Apagar</a>
-                    @endguest
+            <div class="col-md-4">
+                <div class="card">
+                    <a href="/pacotes/{{$pacote->id}}"><img class="card-img-top img-thumbnail img-fluid" src="/storage/{{ $pacote->thumbnail->url }}" alt="Card image cap"></a>
+                    <div class="card-body">
+                        <a href="/pacotes/{{$pacote->id}}"><h5 class="card-title">{{ $pacote->nome }}</h5></a>
+                        <p class="card-text"><small class="text-muted">{{ $pacote->caracteristica1 }}</small></p>
+                        <p class="card-text"><small class="text-muted">{{ $pacote->caracteristica2 }}</small></p>
+                        <p class="card-text"><small class="text-muted">{{ $pacote->caracteristica3 }}</small></p>
+                        <p class="card-text">{{$pacote->preco}} em até {{ $pacote->parcelas }}x</p>
+                        @guest
+                            @if (Route::has('register'))
+                            @endif
+                            @else
+                            <a href="/pacotes/edit/{{$pacote->id}}"><i class="fas fa-edit"></i> Editar/Apagar</a>
+                        @endguest
+                    </div>
                 </div>
             </div>
         @endforeach
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -1,66 +1,76 @@
 @include('layouts.head')
 @include('layouts.header_mini')
 <!-- por nav -->
-<div>
-    <div>
-        {{-- Exibe mensagem de sucesso ou de erro caso haja. --}}
-        @if( \Session::has('error') )
-            @foreach(session()->get('error') as $key => $ms)
-                <span id="{{ $key }}error" class="badge badge-danger badge-pill">
-                    {{ $ms }}
-                    <a id="excluir" onClick="excluirElement('{{ $key }}error')"><i class="fa fa-times" aria-hidden="true"></i></a>
+<div class="container">
+    <h2 class="d-flex justify-content-center">Editar Post</h2>
+    <hr>
+    <div class="col-lg-12 d-flex justify-content-center">
+        <div id="formContent">
+            {{-- Exibe mensagem de sucesso ou de erro caso haja. --}}
+            @if( \Session::has('error') )
+                @foreach(session()->get('error') as $key => $ms)
+                    <span id="{{ $key }}error" class="badge badge-danger badge-pill">
+                        {{ $ms }}
+                        <a id="excluir" onClick="excluirElement('{{ $key }}error')"><i class="fa fa-times" aria-hidden="true"></i></a>
+                    </span>
+                @endforeach
+            @endif
+            @if( \Session::has('message') )
+                <span id="success" class="badge badge-success badge-pill">
+                    {{ \Session::get('message') }}
+                        <a id="excluir" onClick="excluirElement('success')"><i class="fa fa-times" aria-hidden="true"></i></a>
                 </span>
-            @endforeach
-        @endif
-        @if( \Session::has('message') )
-            <span id="success" class="badge badge-success badge-pill">
-                {{ \Session::get('message') }}
-                    <a id="excluir" onClick="excluirElement('success')"><i class="fa fa-times" aria-hidden="true"></i></a>
-            </span>
-        @endif
-        <hr>
-        <form method="POST" action="{{ route('posts.update', $post->id) }}" enctype="multipart/form-data">
-            @csrf
-            @method('put')
-            <input type="text" placeholder="Título" name="titulo" value="{{$post->titulo}}" class="form-control">
-            <input type="text" placeholder="Prévia" name="previa" value="{{$post->previa}}" class="form-control">
-            <input type="text" placeholder="Texto" name="texto" value="{{$post->texto}}" class="form-control">
-            <input type="text" placeholder="Tag" name="tag" value="{{$post->tag}}" class="form-control">
-            <div>
-                <button type="submit" id="submit" class="fadeIn fourth btn btn-primary"> Editar </button>
-            </div>
-
-        </form>
-        <form method="POST" action="{{ route('posts.destroy', $post->id) }}" enctype="multipart/form-data">
-            @csrf
-            @method('delete')
-            <button type="submit" id="submit" class="fadeIn fourth btn btn-primary"> Apagar </button>
-        </form>
-        <div class="card-deck">
-            @foreach($fotos as $foto)
-                    <div class="card">
-                    <img class="card-img-top" src="/storage/{{ $foto->url }}" alt="Card image cap">
-                        <div class="card-body">
+            @endif
+            
+            <form method="POST" action="{{ route('posts.update', $post->id) }}" enctype="multipart/form-data">
+                @csrf
+                @method('put')
+                <input type="text" placeholder="Título" name="titulo" value="{{$post->titulo}}" class="form-control">
+                <input type="text" placeholder="Prévia" name="previa" value="{{$post->previa}}" class="form-control">
+                <input type="text" placeholder="Texto" name="texto" value="{{$post->texto}}" class="form-control">
+                <input type="text" placeholder="Tag" name="tag" value="{{$post->tag}}" class="form-control">
+                <div class="d-flex justify-content-center" id="formFooter">
+                    <button type="submit" id="submit" class="fadeIn fourth btn btn-primary"> Editar </button>
+                    <button class="fadeIn fourth btn btn-primary" href="{{ route('posts.destroy', $post->id) }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('delete-form').submit();"> 
+                        Apagar
+                    </button>
+                </div>
+            </form>
+            <form id="delete-form" method="POST" action="{{ route('posts.destroy', $post->id) }}" enctype="multipart/form-data" style="display:none;">
+                @csrf
+                @method('delete')
+            </form>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        @foreach($fotos as $foto)
+            <div class="col-md-4">
+                <div class="card">
+                    <img class="card-img-top img-thumbnail img-fluid" src="/storage/{{ $foto->url }}" alt="Card image cap">
+                    <div class="card-body">
                         <form method="POST" action="{{ route('fotos.update', $foto->id) }}" enctype="multipart/form-data">
                             @csrf
                             @method('put')
                             <input type="text" placeholder="Nome da Foto" name="nomeFoto" value="{{$foto->nome}}" class="form-control">
-                            <input type="text" placeholder="Thumbnail (V ou F)" name="thumbnail" value="{{$foto->thumbnail}}" class="form-control">
-                        </div>
-                        <button type="submit" id="submit" class="fadeIn fourth btn btn-primary"> Atualizar </button>
+                            <button type="submit" id="submit" class="fadeIn fourth btn btn-primary"> Atualizar </button>
+                            <button class="fadeIn fourth btn btn-primary" href="{{ route('fotos.destroy', $foto->id) }}" 
+                                onclick="event.preventDefault();
+                                document.getElementById('delete-foto{{$foto->id}}').submit();"> 
+                                Apagar
+                            </button>
                         </form>
-                        <form method="POST" action="{{ route('fotos.destroy', $foto->id) }}" enctype="multipart/form-data">
+                        <form id="delete-foto{{$foto->id}}" method="POST" action="{{ route('fotos.destroy', $foto->id) }}" enctype="multipart/form-data">
                             @csrf
                             @method('delete')
-                            <button type="submit" id="submit" class="fadeIn fourth btn btn-primary"> Apagar </button>
                         </form>
-
                     </div>
-                
-            
-            @endforeach
-          </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 </div>
-
 @include('layouts.footer')
