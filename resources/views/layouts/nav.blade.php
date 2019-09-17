c23b1dd5229a79a108037f2c3c4b2fa662c54fee
José Luis Barboza <luis_bg154@hotmail.com>
2019-09-16 21:08:41 -0500

    algumas coisitas

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -14,7 +14,7 @@
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="#">
-                        Bonito
+                        Pantanal Sul 
                     </a>
                     <a class="dropdown-item" href="#">
                         Peru
