972c50271c25f37cd5c6a7e53f7032637c1b7838
krdg10 <krdg10@live.com>
2019-10-09 17:06:23 -0400

    margin blade login e register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card marginCard">
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
